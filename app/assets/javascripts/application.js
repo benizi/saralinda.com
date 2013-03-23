ada3bead6c0db3c01deeae0d9d83bc4252e7862b
Benjamin R. Haskell <benizi@benizi.com>
2013-03-23 12:56:19 -0400

    Fixes the countdown timer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,36 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var Countdown = {};
+Countdown.timer = function() {
+  if (!Countdown.stop) Countdown.stop = new Date(Date.UTC(2013, 2, 23, 22, 0, 0));
+  var now = (new Date).getTime();
+  var diff = Countdown.stop.getTime() - now;
+  diff = Math.floor(diff/1000);
+  var parts = [];
+  var sinceago = '';
+  if (diff > 0) {
+    sinceago = 'to go';
+  } else {
+    sinceago = 'ago';
+    diff = -1 * diff;
+  }
+  var segments = [
+    {name:'week',time:86400*7},
+    {name:'day',time:86400},
+    {name:'hour',time:3600},
+    {name:'minute',time:60},
+    {name:'second',time:1}
+  ];
+  $.each(segments, function(i,segment) {
+    var count = Math.floor(diff / segment.time);
+    if (count > 0) parts.push('' + count + ' ' + segment.name + ((count != 1) ? 's' : ''));
+    diff -= count * segment.time;
+  });
+  $('.countdown').text(parts.join(', ') + ' ' + sinceago);
+};
+
+$(function() {
+  setInterval(Countdown.timer, 100)
+});
